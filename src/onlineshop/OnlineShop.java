d23197b717272bc41dfc2fa8350cdef5726d4499
Mohamed Almukhtar <mohamedalmukhtar870@gmail.com>
2020-04-12 19:02:14 -0700

    Initial commit

---
+++ src/onlineshop/OnlineShop.java
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------- 
+// AUTHOR: Mohamed Mokhtar
+// FILENAME: OnlineShop.java
+// SPECIFICATION: Final Project (Online Shop Application) class
+// FOR: CSE 110 - Final Project
+// TIME SPENT: -
+//-----------------------------------------------------------*/
+
+package onlineshop;
+
+public class OnlineShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
