e3e084da899dd9e17c1e47eb5582aa5002d901d3
Mohamed Almukhtar <mohamedalmukhtar870@gmail.com>
2020-04-12 20:14:06 -0700

    Adds first definition of entity classes

--- src/onlineshop/Customer.java
+++ src/onlineshop/Customer.java
@@ -13,4 +13,60 @@ package onlineshop;
  */
 public class Customer {
     
+    private String firstName;
+    private String lastName;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private int zip;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+    
+    
 }
--- src/onlineshop/Employee.java
+++ src/onlineshop/Employee.java
@@ -5,8 +5,7 @@
 // FOR: CSE 110 - Final Project
 // TIME SPENT: -
 //-----------------------------------------------------------*/
- * Each line should be prefixed with  * 
- */
+
 
 package onlineshop;
 
@@ -17,5 +16,40 @@ package onlineshop;
 
 
 public class Employee {
+    
+    protected int ID;
+    protected String firstName;
+    protected String lastName;
+
+    public Employee(int ID, String firstName, String lastName) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
     
 }
